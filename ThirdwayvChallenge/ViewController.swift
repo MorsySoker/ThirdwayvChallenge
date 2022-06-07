e2a805c7edd1ebc6cec3003601e5520f5c6cb70f
Morsy Mohamed Elsokary <morsysoker@gmail.com>
2022-06-07 22:12:06 +0200

    Initial Commit

---
+++ ThirdwayvChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThirdwayvChallenge
+//
+//  Created by MorsyElsokary on 07/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
